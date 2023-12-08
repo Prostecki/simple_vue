c52b8de986266ccd173a587052b6a6d574e30f63
Mark Taratynov <mark.taratynov@gmail.com>
2023-12-08 13:01:39 +0100

    added comments what I"ve done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="app">
-        <!-- submit prevent - -->
+        <!-- submit prevent - stops a default behavior of webpage (reload page after submit button)-->
         <form @submit.prevent>
+
             <h4>Create a post</h4>
-            <input @input="title = $event.target.value" v-bind:value="title" class="input" type="text" placeholder="Title">
-            <input @input="body = $event.target.value" v-bind:value="body" class="input" type="text" placeholder="Description">
-            <button class="btn" @click="createPost">Create</button>
+
+            <!-- @input - binding the input value to the 'title' data property -->
+            <input 
+            @input="title = $event.target.value" 
+            v-bind:value="title" 
+            class="input" 
+            type="text" 
+            placeholder="Title">
+
+            <!-- @input - binding the input value to the 'body' data property -->
+            <input 
+            @input="body = $event.target.value" 
+            v-bind:value="body" 
+            class="input" 
+            type="text" 
+            placeholder="Description">
+
+            <button 
+            class="btn" 
+            @click="createPost">
+            Create
+            </button>
         </form>
+
+        <!-- Iterating over 'posts' array and displaying each post -->
         <div class="post" v-for="post in posts">
             <div><strong>Title:</strong> {{ post.title }}</div>
             <div><strong>Description:</strong> {{ post.body }}</div>
@@ -18,24 +40,31 @@
 export default {
     data() {
         return {
+            // Initial array of posts
             posts: [
                 {id: 1, title: 'Vuejs', body: 'Description of post 1'},
                 {id: 2, title: 'Vuejs', body: 'Description of post 2'},
                 {id: 3, title: 'Vuejs', body: 'Description of post 3'},
                 {id: 4, title: 'Vuejs', body: 'Description of post 4'}
             ],
+            // Data properties for form inputs
             title: '',
             body: '',
         }
     },
     methods: {
+        // Method to create a new post
         createPost() {
+            // Create a new post object
             const newPost = {
                 id: Date.now(),
                 title: this.title,
                 body: this.body,
             }
+            // Push the new post to the 'posts' array
             this.posts.push(newPost);
+            
+            // Clear the form inputs
             this.title = '';
             this.body = '';
         },
