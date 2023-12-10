7ecbf069f1beb5a80c13b0acc650c8d3e3c55e4d
Mark Taratynov <mark.taratynov@gmail.com>
2023-12-10 13:45:59 +0100

    delete button

---
+++ src/components/PostItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- Iterating over "posts" array and displaying each post -->
+    <div class="post">
+        <div>
+            <div><strong>Title</strong> {{ post.title }} </div>
+            <div><strong>Description</strong> {{ post.body }} </div>
+        </div>
+        <div class="post__btns">
+            <button>
+                Delete
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            post: {
+                type: Object,
+                requried: true,
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .post {
+        display: flex;
+        align-items: center;
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
+        box-shadow: inset 1px 1px 5px black;
+        justify-content: space-between;
+    }
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,16 +1,19 @@
 <!-- The syntax includes of using template tag for structurizing html contents -->
 <template>
-    <!-- Iterating over "posts" array and displaying each post -->
-    <div class="post" v-for="post in posts">
-        <div><strong>Title:</strong> {{ post.title }}</div>
-        <div><strong>Description:</strong> {{ post.body }}</div>
+    <!-- Iterating over posts array and displaying each post -->
+    <div>
+        <h3>Users list</h3>
+        <PostItem 
+        v-for="post in posts"
+        :post="post" />
     </div>
 </template>
 
 <script>
-
+import PostItem from "@/components/PostItem.vue"
 // Need to export the components in order to use in App.vue
 export default {
+    components: {PostItem},
     props: {
         posts: {
             type: Array,
@@ -23,10 +26,5 @@ export default {
 
 <!-- Styles is using only for this component -->
 <style scoped>
-.post {
-        padding: 15px;
-        border: 2px solid teal;
-        margin-top: 15px;
-        box-shadow: inset 1px 1px 5px black;
-    }
+
 </style>
