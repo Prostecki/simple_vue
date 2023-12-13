88d90166eea75aba69ad6e5b183d3dc5d6e87590
Mark Taratynov <mark.taratynov@gmail.com>
2023-12-13 11:57:18 +0100

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <!-- The syntax includes of using template tag for structurizing html contents -->
 <template>
     <div class="app">
-        <PostForm 
-        @create="createPost"
-        />
+        <h1>Page with posts</h1>
+        <MyButton
+        @click="showDialog"
+        >Create a user</MyButton>
+        <MyDialog v-model:show="dialogVisible">
+            <PostForm
+            @create="createPost" 
+            />
+        </MyDialog>
         <PostList 
         v-bind:posts="posts"
         @remove="removePost"
@@ -16,12 +22,14 @@
 // Need to add .vue for displaying components!
 import PostForm from "@/components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
+import MyDialog from "@/components/UI/MyDialog.vue";
 
 // Need to export default components in order to use all of them wherever
 export default {
     components: {
         PostList, 
-        PostForm
+        PostForm,
+        MyDialog
     },
     data() {
         return {
@@ -31,7 +39,8 @@ export default {
                 {id: 2, title: 'Vuejs', body: 'Description of post 2'},
                 {id: 3, title: 'Vuejs', body: 'Description of post 3'},
                 {id: 4, title: 'Vuejs', body: 'Description of post 4'}
-            ]
+            ],
+            dialogVisible: false
         }
     },
     methods: {
@@ -40,8 +49,11 @@ export default {
             this.posts.push(post);
         },
         removePost(post) {
-            this.posts = this.posts.filter(p => p.id !== post.id)
+            this.posts = this.posts.filter(p => p.id !== post.id);
         },
+        showDialog() {
+            this.dialogVisible = true;
+        }
     }
 }
 </script>
